6079abf6a86e122cdf0d1c8773cdce4598b52f52
Allan <allan.oc@outlook.com>
2021-01-09 23:52:19 -0300

    desafio_automacao

--- cypress/support/pageobjects/HomePage.js
+++ cypress/support/pageobjects/HomePage.js
@@ -1,30 +1,29 @@
 /// <reference types="Cypress" />
 
-import HomeElements from '../elements/HomeElements';
+import HomeElements from '../elements/HomeElements'
 
-const homeElements = new HomeElements;
-//const url = Cypress.config("baseUrl");
+const homeElements = new HomeElements
 
 class HomePage {
     // Acessa a page que será testada
     acessarPage() {
-        cy.visit('https://shop.samsung.com.br/');
+        cy.visit('https://shop.samsung.com.br/')
     }
 
     clicarCloseCookies(){
         if(cy.get(homeElements.closeCookies()) == true){
-            cy.get(homeElements.closeCookies()).click();
+            cy.get(homeElements.closeCookies()).click()
         }
     }
 
     clicarCloseNotifications(){
-        cy.get(homeElements.closeNotifications()).click();
+        cy.get(homeElements.closeNotifications()).click()
     }
 
     // Clica no elemento que acessa a página de account do site
     clicarBotaoAccount() {
-        cy.get(homeElements.botaoAccount()).click();
+        cy.get(homeElements.botaoAccount()).click()
     }
 }
 
-export default HomePage;
+export default HomePage
